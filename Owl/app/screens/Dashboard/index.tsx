a075618ee87625fac5bd3e11cb0598e45796e502
Yannik Wojcicki <ywojcicki@mailbox.org>
2020-02-29 15:05:00 +0100

    Add dashboard screen

---
+++ Owl/app/screens/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styled from 'styled-components';
+
+const Dashboard = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default Dashboard;
