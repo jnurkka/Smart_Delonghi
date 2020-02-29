e4284e380688f8e9e670950127441bab5b85357b
Jaakko Nurkka <jaakkonurkka@gmail.com>
2020-02-29 19:29:43 +0100

    Add gpio change listening

---
+++ coffeeserver/gpio/status.js
@@ -0,0 +1,7 @@
+const isPowerOn = false;
+const isWaterEmpty = false;
+const isTrashFull = false;
+
+export const onPowerLightChange = event => console.log(event);
+export const onWaterLightChange = event => console.log(event);
+export const onTrashLightChange = event => console.log(event);
