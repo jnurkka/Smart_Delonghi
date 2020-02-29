05593c5a88673034637d4b8e33c7f7961e2c8c26
Jaakko Nurkka <jaakkonurkka@gmail.com>
2020-02-29 14:58:30 +0100

    Initialise server

---
+++ coffeeserver/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var express_graphql = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`);
+
+var root = {
+    message: () => 'Hello World!'
+};
+
+var app = express();
+
+app.use('/graphql', express_graphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+
+app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
